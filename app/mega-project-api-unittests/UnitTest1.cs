c6404303423eb304458e5d66db2d7c1577d29c5c
Jared Holgate <jaredholgate@microsoft.com>
2023-07-31 14:16:56 +0100

    Add test project (#2)

---
+++ app/mega-project-api-unittests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace mega_project_api_unittests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
--- app/mega-project-api/ExampleClass.cs
+++ app/mega-project-api/ExampleClass.cs
@@ -6,5 +6,38 @@ namespace mega_project_api
         {
             return "Hello World!";
         }
+
+        // Get fibonacci sequence for gove length
+        public int[] Fibonacci(int length)
+        {
+            int[] sequence = new int[length];
+            if (length == 0)
+            {
+                return sequence;
+            }
+            else if (length == 1)
+            {
+                sequence[0] = 0;
+                return sequence;
+            }
+            else if (length == 2)
+            {
+                sequence[0] = 0;
+                sequence[1] = 1;
+                return sequence;
+            }
+            else
+            {
+                sequence[0] = 0;
+                sequence[1] = 1;
+                for (int i = 2; i < length; i++)
+                {
+                    sequence[i] = sequence[i - 1] + sequence[i - 2];
+                }
+                return sequence;
+            }
+        }
+
+
     }
 }
