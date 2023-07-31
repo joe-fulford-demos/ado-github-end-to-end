fcb336b1ef71ac323ca3e11f621ed611d805ac18
Jared Holgate <jaredholgate@microsoft.com>
2023-07-31 14:22:38 +0100

    Add password to code

--- app/mega-project-api/BadCode.cs
+++ app/mega-project-api/BadCode.cs
@@ -4,7 +4,7 @@ namespace mega_project_api
     {
         public void UpdateDatabase(string somethingToUpdate)
         {
-            var connection = new SqlConnection("Server=.;Database=master;Trusted_Connection=True;");       
+            var connection = new SqlConnection("Server=.;Database=master;Trusted_Connection=True;User Id=sa;Password=ABC!5$5556pO;");       
             var sql = $"UPDATE [dbo].[ExampleTable] SET [ExampleColumn] = {somethingToUpdate}";
             var command = new SqlCommand(sql, connection);
             command.ExecuteNonQuery();
